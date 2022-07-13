62cb348eb2bb127b458062face621cf0d3372910
Shubham <Shubham@host.docker.internal>
2022-07-13 13:11:00 +0530

    added getter setter

--- StudentInformationSystem/src/model/Customer.java
+++ StudentInformationSystem/src/model/Customer.java
@@ -2,12 +2,23 @@ package model;
 
 public class Customer {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int customerId;
-		String customerName;
+	int customerId;
+	String customerName;
 
-		
+	public int getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+
+	public String getCustomerName() {
+		return customerName;
+	}
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
 	}
 
 }
