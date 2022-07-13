59a4513c97b6f717e1b24c357d84a3bdcbec04cb
Shubham <Shubham@host.docker.internal>
2022-07-13 12:26:06 +0530

    Added Customer.java file

---
+++ StudentInformationSystem/src/model/Customer.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Customer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int customerId;
+		String customerName;
+
+		
+	}
+
+}
