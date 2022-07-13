ecede986ac5e734c199bc4fdc22cb2d4fee1096c
Shubham <Shubham@host.docker.internal>
2022-07-13 13:31:45 +0530

    Added account.java in branch

---
+++ StudentInformationSystem/src/model/account.java
@@ -0,0 +1,20 @@
+package model;
+
+public class account {
+	int accountNo;
+	String accountName;
+	public int getAccountNo() {
+		return accountNo;
+	}
+	public void setAccountNo(int accountNo) {
+		this.accountNo = accountNo;
+	}
+	public String getAccountName() {
+		return accountName;
+	}
+	public void setAccountName(String accountName) {
+		this.accountName = accountName;
+	}
+	
+
+}
